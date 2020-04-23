04a8f79ee2bace4f6e71eb589ac77b6f5ad847f6
Gleb <Dreamer_Gleb_Shevchenko@users.noreply.github.com>
2020-04-23 16:55:53 +0300

    ф

--- mcustore/Form1.Designer.cs
+++ mcustore/Form1.Designer.cs
@@ -28,12 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.password_tb = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.user_tb = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -55,18 +56,9 @@
             this.label2.ForeColor = System.Drawing.Color.White;
             this.label2.Location = new System.Drawing.Point(219, 314);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(261, 48);
+            this.label2.Size = new System.Drawing.Size(252, 47);
             this.label2.TabIndex = 1;
             this.label2.Text = "Пользователь";
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.BackColor = System.Drawing.Color.White;
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(677, 309);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(353, 44);
-            this.comboBox1.TabIndex = 2;
             // 
             // label3
             // 
@@ -76,16 +68,16 @@
             this.label3.ForeColor = System.Drawing.Color.White;
             this.label3.Location = new System.Drawing.Point(219, 383);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(145, 48);
+            this.label3.Size = new System.Drawing.Size(140, 47);
             this.label3.TabIndex = 3;
             this.label3.Text = "Пароль";
             // 
-            // textBox1
+            // password_tb
             // 
-            this.textBox1.Location = new System.Drawing.Point(677, 381);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(353, 41);
-            this.textBox1.TabIndex = 4;
+            this.password_tb.Location = new System.Drawing.Point(677, 381);
+            this.password_tb.Name = "password_tb";
+            this.password_tb.Size = new System.Drawing.Size(353, 41);
+            this.password_tb.TabIndex = 4;
             // 
             // button1
             // 
@@ -97,6 +89,14 @@
             this.button1.TabIndex = 5;
             this.button1.Text = "Войти";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // user_tb
+            // 
+            this.user_tb.Location = new System.Drawing.Point(677, 320);
+            this.user_tb.Name = "user_tb";
+            this.user_tb.Size = new System.Drawing.Size(353, 41);
+            this.user_tb.TabIndex = 6;
             // 
             // Form1
             // 
@@ -106,15 +106,16 @@
             this.BackgroundImage = global::mcustore.Properties.Resources.fon_vxod;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1158, 512);
+            this.Controls.Add(this.user_tb);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.password_tb);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.ForeColor = System.Drawing.Color.Black;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -129,10 +130,10 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox password_tb;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox user_tb;
     }
 }
 
--- mcustore/Form1.cs
+++ mcustore/Form1.cs
@@ -24,7 +24,47 @@ namespace mcustore
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            textBox1.UseSystemPasswordChar = true;
+            password_tb.UseSystemPasswordChar = true;
+            user_tb.Select();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            bool is_password_right = false; // верный ли пароль
+            if (user_tb.Text=="admin") // если вход через учётную запись бухгалтера
+            {
+                    is_password_right = true;
+            }
+            else if (user_tb.Text == "user") // если вход через учётную запись администратора
+            {
+                if (password_tb.Text == "123456789") // если введённый пароль - верный
+                {
+                    is_password_right = true;
+                }
+            }
+            else if(user_tb.Text == "developer")
+            {
+                if (password_tb.Text == "987654321") // если введённый пароль - верный
+                {
+                    is_password_right = true;
+                }
+            }
+
+            if (is_password_right) // если введённый пароль - верный
+            {
+                this.Close();//закрываем текущую форму
+                Work_Window WorkForm = new Work_Window(); // создаём главную форму
+                WorkForm.Owner = this; // устанавливаем владельца главной формы - эту форму
+                //WorkForm.IsAdmin = radioButton_administrator.Checked; // отправляем в главную форму информацию о том, под какой учётной записью выполнен вход
+                
+                Hide(); // скрываем текущую форму
+                WorkForm.Show(); // показываем главную форму
+            }
+            else // если введённый пароль - неверный
+            {
+                //textBox_password.Clear(); // очищаем поле для ввода пароля
+                MessageBox.Show("Неверный пароль или логин!", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error); // показываем сообщение об ошибке
+            }
         }
     }
 }
