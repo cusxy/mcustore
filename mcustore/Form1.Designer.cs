d5efb2d6b0fcddd41240f7fea478ef16e2bd2314
Gleb <Dreamer_Gleb_Shevchenko@users.noreply.github.com>
2020-04-23 18:56:07 +0300

    форма входа обновлена

--- mcustore/Form1.Designer.cs
+++ mcustore/Form1.Designer.cs
@@ -35,6 +35,9 @@
             this.password_tb = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.user_tb = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -42,11 +45,13 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Comic Sans MS", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(372, 9);
+            this.label1.Location = new System.Drawing.Point(413, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(480, 56);
+            this.label1.Size = new System.Drawing.Size(387, 45);
             this.label1.TabIndex = 0;
             this.label1.Text = "ДОБРО ПОЖАЛОВАТЬ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Resize += new System.EventHandler(this.Label1_Resize);
             // 
             // label2
             // 
@@ -54,9 +59,9 @@
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Comic Sans MS", 19.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(219, 314);
+            this.label2.Location = new System.Drawing.Point(191, 319);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(252, 47);
+            this.label2.Size = new System.Drawing.Size(207, 38);
             this.label2.TabIndex = 1;
             this.label2.Text = "Пользователь";
             // 
@@ -66,46 +71,86 @@
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Comic Sans MS", 19.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(219, 383);
+            this.label3.Location = new System.Drawing.Point(191, 385);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(140, 47);
+            this.label3.Size = new System.Drawing.Size(115, 38);
             this.label3.TabIndex = 3;
             this.label3.Text = "Пароль";
             // 
             // password_tb
             // 
-            this.password_tb.Location = new System.Drawing.Point(677, 381);
+            this.password_tb.Location = new System.Drawing.Point(649, 388);
             this.password_tb.Name = "password_tb";
-            this.password_tb.Size = new System.Drawing.Size(353, 41);
+            this.password_tb.Size = new System.Drawing.Size(353, 35);
             this.password_tb.TabIndex = 4;
             // 
             // button1
             // 
-            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(107)))), ((int)(((byte)(219)))));
             this.button1.Font = new System.Drawing.Font("Comic Sans MS", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(452, 446);
+            this.button1.Location = new System.Drawing.Point(733, 448);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(269, 54);
             this.button1.TabIndex = 5;
             this.button1.Text = "Войти";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.Button1_Click);
+            this.button1.MouseLeave += new System.EventHandler(this.ButtonLigthLeave);
+            this.button1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ButtonLigthMove);
             // 
             // user_tb
             // 
-            this.user_tb.Location = new System.Drawing.Point(677, 320);
+            this.user_tb.Location = new System.Drawing.Point(649, 322);
             this.user_tb.Name = "user_tb";
-            this.user_tb.Size = new System.Drawing.Size(353, 41);
+            this.user_tb.Size = new System.Drawing.Size(353, 35);
             this.user_tb.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(107)))), ((int)(((byte)(219)))));
+            this.button2.Font = new System.Drawing.Font("Comic Sans MS", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(199, 448);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(269, 54);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Регистрация";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            this.button2.MouseLeave += new System.EventHandler(this.ButtonLigthLeave);
+            this.button2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ButtonLigthMove);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Comic Sans MS", 19.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(192, 253);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(222, 38);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Код активации";
+            this.label4.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(649, 253);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(353, 35);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.Visible = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 36F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.BackgroundImage = global::mcustore.Properties.Resources.fon_vxod;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1158, 512);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.user_tb);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.password_tb);
@@ -134,6 +179,9 @@
         private System.Windows.Forms.TextBox password_tb;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox user_tb;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- mcustore/Form1.cs
+++ mcustore/Form1.cs
@@ -17,54 +17,116 @@ namespace mcustore
             InitializeComponent();
         }
 
-        private void Label1_Click(object sender, EventArgs e)
-        {
-
-        }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             password_tb.UseSystemPasswordChar = true;
             user_tb.Select();
         }
+        /// <summary>
+        /// цвет при наведение на кнопку
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonLigthMove(object sender, MouseEventArgs e)
+        {
+            Button t = sender as Button;
+            t.BackColor = Color.FromArgb(255, 255, 255);
+        }
+
+        /// <summary>
+        /// Цвет при ухождение с кнопки
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonLigthLeave(object sender, EventArgs e)
+        {
+            Button t = sender as Button;
+            t.BackColor = Color.FromArgb(37, 107, 219);
+        }
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            bool is_password_right = false; // верный ли пароль
-            if (user_tb.Text=="admin") // если вход через учётную запись бухгалтера
+            if(registr)
             {
-                    is_password_right = true;
+                if(textBox1.Text == "AAAA-AAAA")
+                {
+                    Registr_User();
+                }
+                else
+                {
+                    textBox1.Clear();
+                    MessageBox.Show("Неверный код активации!", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error); // показываем сообщение об ошибке
+                }
             }
-            else if (user_tb.Text == "user") // если вход через учётную запись администратора
+            else
             {
-                if (password_tb.Text == "123456789") // если введённый пароль - верный
+                bool is_password_right = false; // верный ли пароль
+                if (user_tb.Text == "admin") // если вход через учётную запись бухгалтера
                 {
                     is_password_right = true;
                 }
-            }
-            else if(user_tb.Text == "developer")
-            {
-                if (password_tb.Text == "987654321") // если введённый пароль - верный
+                else if (user_tb.Text == "user") // если вход через учётную запись администратора
                 {
-                    is_password_right = true;
+                    if (password_tb.Text == "123456789") // если введённый пароль - верный
+                    {
+                        is_password_right = true;
+                    }
                 }
-            }
+                else if (user_tb.Text == "developer")
+                {
+                    if (password_tb.Text == "987654321") // если введённый пароль - верный
+                    {
+                        is_password_right = true;
+                    }
+                }
 
-            if (is_password_right) // если введённый пароль - верный
-            {
-                this.Close();//закрываем текущую форму
-                Work_Window WorkForm = new Work_Window(); // создаём главную форму
-                WorkForm.Owner = this; // устанавливаем владельца главной формы - эту форму
-                //WorkForm.IsAdmin = radioButton_administrator.Checked; // отправляем в главную форму информацию о том, под какой учётной записью выполнен вход
-                
-                Hide(); // скрываем текущую форму
-                WorkForm.Show(); // показываем главную форму
+                if (is_password_right) // если введённый пароль - верный
+                {
+                    this.Close();//закрываем текущую форму
+                    Work_Window WorkForm = new Work_Window(); // создаём главную форму
+                    WorkForm.Owner = this; // устанавливаем владельца главной формы - эту форму
+                                           //WorkForm.IsAdmin = radioButton_administrator.Checked; // отправляем в главную форму информацию о том, под какой учётной записью выполнен вход
+
+                    Hide(); // скрываем текущую форму
+                    WorkForm.Show(); // показываем главную форму
+                }
+                else // если введённый пароль - неверный
+                {
+                    password_tb.Clear(); // очищаем поле для ввода пароля
+                    MessageBox.Show("Неверный пароль или логин!", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error); // показываем сообщение об ошибке
+                }
             }
-            else // если введённый пароль - неверный
+        }
+        bool registr = false;
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if(button2.Text == "Регистрация")
             {
-                password_tb.Clear(); // очищаем поле для ввода пароля
-                MessageBox.Show("Неверный пароль или логин!", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error); // показываем сообщение об ошибке
+                button1.Text = "Зарегистрировать";
+                button2.Text = "Отмена";
+                registr = true;
+                label1.Text = "РЕГИСТРАЦИЯ";
+                label4.Visible = textBox1.Visible = true;
             }
+            else
+            {
+                button1.Text = "Войти";
+                button2.Text = "Регистрация";
+                registr = false;
+                label1.Text = "ДОБРО ПОЖАЛОВАТЬ";
+                label4.Visible = textBox1.Visible = false;
+            }
+        }
+
+        private void Label1_Resize(object sender, EventArgs e)
+        {
+            label1.Location = new Point(this.Width / 2 - label1.Width / 2, 9);
+        }
+
+        private void Registr_User()
+        {
+
         }
     }
 }
