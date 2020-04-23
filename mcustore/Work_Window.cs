aa542225909273df1e8c770338224311adc493bf
Gleb <Dreamer_Gleb_Shevchenko@users.noreply.github.com>
2020-04-23 15:41:14 +0300

    плюс форма

---
+++ mcustore/Work_Window.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mcustore
+{
+    public partial class Work_Window : Form
+    {
+        public Work_Window()
+        {
+            InitializeComponent();
+        }
+    }
+}
