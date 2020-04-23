a8a88a505e05bb7a0b3d9080b3f7d89ad05878a4
Gleb <Dreamer_Gleb_Shevchenko@users.noreply.github.com>
2020-04-23 16:22:01 +0300

    форма входа

--- mcustore/Form1.Designer.cs
+++ mcustore/Form1.Designer.cs
@@ -33,6 +33,7 @@
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -40,11 +41,11 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Comic Sans MS", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(530, 9);
+            this.label1.Location = new System.Drawing.Point(372, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(141, 56);
+            this.label1.Size = new System.Drawing.Size(480, 56);
             this.label1.TabIndex = 0;
-            this.label1.Text = "ВХОД";
+            this.label1.Text = "ДОБРО ПОЖАЛОВАТЬ";
             // 
             // label2
             // 
@@ -52,7 +53,7 @@
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Comic Sans MS", 19.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(261, 313);
+            this.label2.Location = new System.Drawing.Point(219, 314);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(261, 48);
             this.label2.TabIndex = 1;
@@ -62,7 +63,7 @@
             // 
             this.comboBox1.BackColor = System.Drawing.Color.White;
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(719, 308);
+            this.comboBox1.Location = new System.Drawing.Point(677, 309);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(353, 44);
             this.comboBox1.TabIndex = 2;
@@ -73,7 +74,7 @@
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Comic Sans MS", 19.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(261, 382);
+            this.label3.Location = new System.Drawing.Point(219, 383);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(145, 48);
             this.label3.TabIndex = 3;
@@ -81,10 +82,21 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(719, 380);
+            this.textBox1.Location = new System.Drawing.Point(677, 381);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(353, 41);
             this.textBox1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(452, 446);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(269, 54);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Войти";
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
@@ -94,6 +106,7 @@
             this.BackgroundImage = global::mcustore.Properties.Resources.fon_vxod;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1158, 512);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.comboBox1);
@@ -102,7 +115,9 @@
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.ForeColor = System.Drawing.Color.Black;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Вход";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
@@ -117,6 +132,7 @@
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
