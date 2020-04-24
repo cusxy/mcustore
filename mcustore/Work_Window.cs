a191f85b397bdf865cab7099999f62a63bd2e573
Gleb <Dreamer_Gleb_Shevchenko@users.noreply.github.com>
2020-04-24 18:00:22 +0300

    ajhvf

--- mcustore/Work_Window.cs
+++ mcustore/Work_Window.cs
@@ -266,6 +266,7 @@ namespace mcustore
         private void Button2_Click(object sender, EventArgs e)
         {
             DataBaseClass.CreateNewOrder(textBox1.Text, mass.ToList(), array.ToList());
+            Work_Window_Load(sender, e);
         }
     }
 }
