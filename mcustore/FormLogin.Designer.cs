be36340cc2fdd153e6f9eb5874cdb99004ca97c3
Gleb <Dreamer_Gleb_Shevchenko@users.noreply.github.com>
2020-04-26 12:47:27 +0300

    Внес некоторые изменения по дизайну формы входа

--- mcustore/FormLogin.Designer.cs
+++ mcustore/FormLogin.Designer.cs
@@ -37,8 +37,8 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(636, 525);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.textBox1.Location = new System.Drawing.Point(619, 374);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(7);
             this.textBox1.MaxLength = 32;
             this.textBox1.Name = "textBox1";
             this.textBox1.PasswordChar = '*';
@@ -53,15 +53,15 @@
             this.label1.Location = new System.Drawing.Point(390, 31);
             this.label1.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(450, 39);
+            this.label1.Size = new System.Drawing.Size(436, 38);
             this.label1.TabIndex = 1;
             this.label1.Text = "ДОБРО ПОЖАЛОВАТЬ!";
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(317, 602);
-            this.button1.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.button1.Location = new System.Drawing.Point(300, 451);
+            this.button1.Margin = new System.Windows.Forms.Padding(7);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(632, 45);
             this.button1.TabIndex = 2;
@@ -74,7 +74,7 @@
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(312, 525);
+            this.label2.Location = new System.Drawing.Point(295, 374);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(176, 29);
             this.label2.TabIndex = 3;
@@ -87,15 +87,16 @@
             this.BackColor = System.Drawing.Color.White;
             this.BackgroundImage = global::mcustore.Properties.Resources.fon_vxod;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1182, 753);
+            this.ClientSize = new System.Drawing.Size(1182, 553);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Font = new System.Drawing.Font("Georgia", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.Margin = new System.Windows.Forms.Padding(7);
+            this.MaximizeBox = false;
             this.Name = "FormLogin";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Вход";
